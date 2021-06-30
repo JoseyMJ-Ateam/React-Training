6bc7ecbdcd64432376d11747b95a3f82ce5cb797
joseymj90 <joseymj90@gmail.com>
2021-06-30 18:57:40 +0530

    Loader Calendar Toggle Components done

--- src/Container/Layouts/SidebarMenus/dashoard.jsx
+++ src/Container/Layouts/SidebarMenus/dashoard.jsx
@@ -1,10 +1,23 @@
 
+import { CssBaseline, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles  ({
+    container:{
+        width:'100%',
+        height:'100%',
+        backgroundColor:'white',
+        display:'flex',
+        justifyContent:'center'
+    }
+});
 
 const Dashboard = () => {
+    const classes = useStyles();
     return(
-        <>
+        <div className={classes.container}>
         <h1>Dashboard</h1>
-        </>
+        <CssBaseline />
+        </div>
     )
 }
 export default Dashboard;
